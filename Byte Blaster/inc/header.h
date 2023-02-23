50008e00114f2f525d1fadeb61c62036d38673ee
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2023-02-23 12:45:47 +0200

    Byte Blaster

--- Byte Blaster/inc/header.h
+++ Byte Blaster/inc/header.h
@@ -63,9 +63,9 @@
 #include <string.h>
 
 #include <SDL2/SDL.h>
-#include <SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
+#include "SDL2_image/SDL_image.h"
+#include "SDL2_mixer/SDL_mixer.h"
+#include "SDL2_ttf/SDL_ttf.h"
 
 enum
 {
